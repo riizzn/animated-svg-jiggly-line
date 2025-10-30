6e62a770f91c6415d9f0f39e82b7511f353764c1
Rez <rezhates@gmail.com>
2025-10-30 12:45:43 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='h-screen py-10 text-white '>
+      <div className='h-px w-full relative mt-[50px]'>
+        <svg className='w-full h-[100px] top-[-50px] absolute'>
+          <path></path>
+        </svg>
+      </div>
+    </div>
+  )
+}
+
+export default App
